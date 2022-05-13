1520114818d7acaa2eb519424ed0679da386ef59
salihkilicoglu <salihkilicoglu@gmail.com>
2022-05-13 04:13:55 +0300

    design changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 
 urlpatterns = [
     path('', views.api_home), # /api/
+    path('weather/', include('weather.urls')),
 ]
 
 # token things here
